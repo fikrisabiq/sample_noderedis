8fd42e9855e4ee1f93038f7ddba46d8e356814f0
fikrisabiq <fikrisabiq@gmail.com>
2022-12-09 19:35:03 +0700

    menambahkan error catch

--- services/cache.js
+++ services/cache.js
@@ -9,50 +9,57 @@ const client = redis.createClient({
   retry_strategy: () => 1000,
 });
 
-client.connect().then(() => {
-  client.hGet = util.promisify(client.hGet);
-  const exec = mongoose.Query.prototype.exec;
+client
+  .connect()
+  .then(() => {
+    client.hGet = util.promisify(client.hGet);
+    const exec = mongoose.Query.prototype.exec;
 
-  mongoose.Query.prototype.cache = function (options = { time: 60 }) {
-    this.useCache = true;
-    this.time = options.time;
-    this.hashKey = JSON.stringify(options.key || this.mongooseCollection.name);
+    mongoose.Query.prototype.cache = function (options = { time: 60 }) {
+      this.useCache = true;
+      this.time = options.time;
+      this.hashKey = JSON.stringify(
+        options.key || this.mongooseCollection.name
+      );
 
-    return this;
-  };
+      return this;
+    };
 
-  mongoose.Query.prototype.exec = async function () {
-    if (!this.useCache) {
-      return await exec.apply(this, arguments);
-    }
+    mongoose.Query.prototype.exec = async function () {
+      if (!this.useCache) {
+        return await exec.apply(this, arguments);
+      }
 
-    const key = JSON.stringify({
-      ...this.getQuery(),
-    });
+      const key = JSON.stringify({
+        ...this.getQuery(),
+      });
 
-    const cacheValue = await client.hGet(this.hashKey, key);
+      const cacheValue = await client.hGet(this.hashKey, key);
 
-    if (cacheValue) {
-      const doc = JSON.parse(cacheValue);
+      if (cacheValue) {
+        const doc = JSON.parse(cacheValue);
 
-      console.log('Response from Redis');
-      return Array.isArray(doc)
-        ? doc.map((d) => new this.model(d))
-        : new this.model(doc);
-    }
+        console.log('Response from Redis');
+        return Array.isArray(doc)
+          ? doc.map((d) => new this.model(d))
+          : new this.model(doc);
+      }
 
-    const result = await exec.apply(this, arguments);
-    console.log(this.time);
-    client.hset(this.hashKey, key, JSON.stringify(result));
-    client.expire(this.hashKey, this.time);
+      const result = await exec.apply(this, arguments);
+      console.log(this.time);
+      client.hset(this.hashKey, key, JSON.stringify(result));
+      client.expire(this.hashKey, this.time);
 
-    console.log('Response from MongoDB');
-    return result;
-  };
+      console.log('Response from MongoDB');
+      return result;
+    };
 
-  module.exports = {
-    clearKey(hashKey) {
-      client.del(JSON.stringify(hashKey));
-    },
-  };
-});
+    module.exports = {
+      clearKey(hashKey) {
+        client.del(JSON.stringify(hashKey));
+      },
+    };
+  })
+  .catch((err) => {
+    console.log(err);
+  });
