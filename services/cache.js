03de11e6ea79ad0213c8e90619b6c95d816b4b86
fikrisabiq <fikrisabiq@gmail.com>
2022-12-09 21:37:43 +0700

    hola

--- services/cache.js
+++ services/cache.js
@@ -3,64 +3,66 @@ const redis = require('redis');
 const util = require('util');
 // const keys = require("../config/keys");
 
-const client = redis.createClient({
-  host: 'localhost',
-  port: 6379,
-  retry_strategy: () => 1000,
-});
-
-client
-  .connect()
-  .then(() => {
-    console.log('berhasil tersambung');
-    client.get = util.promisify(client.get);
-    const exec = mongoose.Query.prototype.exec;
-
-    mongoose.Query.prototype.cache = function (options = { time: 5 }) {
-      this.useCache = true;
-      this.time = options.time;
-      this.hashKey = JSON.stringify(
-        options.key || this.mongooseCollection.name
-      );
-
-      return this;
-    };
-
-    mongoose.Query.prototype.exec = async function () {
-      if (!this.useCache) {
-        return await exec.apply(this, arguments);
-      }
-
-      const key = JSON.stringify({
-        ...this.getQuery(),
-      });
-
-      const cacheValue = await client.get(this.hashKey, key);
-
-      if (cacheValue) {
-        const doc = JSON.parse(cacheValue);
-
-        console.log('Response from Redis');
-        return Array.isArray(doc)
-          ? doc.map((d) => new this.model(d))
-          : new this.model(doc);
-      }
-
-      const result = await exec.apply(this, arguments);
-      console.log(this.time);
-      client.hset(this.hashKey, key, JSON.stringify(result));
-      client.expire(this.hashKey, this.time);
-
-      console.log('Response from MongoDB');
-      return result;
-    };
-
-    module.exports = {
-      clearKey(hashKey) {
-        client.del(JSON.stringify(hashKey));
-      },
-    };
-  })
-  .catch((err) => {
-    console.log(err);
+(async () => {
+  const client = redis.createClient({
+    host: 'localhost',
+    port: 6379,
+    retry_strategy: () => 1000,
   });
+
+  client.on('error', function (err) {
+    console.error('Errro encountered: ', err);
+  });
+
+  client.on('connect', () => {
+    console.log('Redis connected');
+  });
+
+  await client.connect();
+
+  client.get = util.promisify(client.get);
+  const exec = mongoose.Query.prototype.exec;
+
+  mongoose.Query.prototype.cache = function (options = { time: 5 }) {
+    this.useCache = true;
+    this.time = options.time;
+    this.hashKey = JSON.stringify(options.key || this.mongooseCollection.name);
+
+    return this;
+  };
+
+  mongoose.Query.prototype.exec = async function () {
+    if (!this.useCache) {
+      return await exec.apply(this, arguments);
+    }
+
+    const key = JSON.stringify({
+      ...this.getQuery(),
+    });
+
+    const cacheValue = await client.get(this.hashKey, key);
+
+    if (cacheValue) {
+      const doc = JSON.parse(cacheValue);
+
+      console.log('Response from Redis');
+      return Array.isArray(doc)
+        ? doc.map((d) => new this.model(d))
+        : new this.model(doc);
+    }
+
+    const result = await exec.apply(this, arguments);
+    console.log(this.time);
+    client.hset(this.hashKey, key, JSON.stringify(result));
+    client.expire(this.hashKey, this.time);
+
+    console.log('Response from MongoDB');
+    return result;
+  };
+
+  module.exports = {
+    clearKey(hashKey) {
+      client.del(JSON.stringify(hashKey));
+    },
+  };
+})();
