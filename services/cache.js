5005a0f68c3c138c3f4747e7be7cd379adf4b06c
fikrisabiq <fikrisabiq@gmail.com>
2022-12-11 02:05:42 +0700

    time

--- services/cache.js
+++ services/cache.js
@@ -15,12 +15,11 @@ const util = require('util');
   client.HGET = util.promisify(client.HGET);
   const exec = mongoose.Query.prototype.exec;
 
-  mongoose.Query.prototype.cache = function (options = { time: 60 }) {
+  mongoose.Query.prototype.cache = function (time = 60) {
     this.useCache = true;
-    this.time = options.time;
+    this.time = time;
     this.hashKey = JSON.stringify(this.mongooseCollection.name);
     console.log(this.time);
-    console.log(options);
 
     return this;
   };
