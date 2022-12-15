602c3ffaa0b2982760fe720e25062ccf64692c05
fikrisabiq <fikrisabiq@gmail.com>
2022-12-15 20:28:21 +0700

    second commit

--- services/cache.js
+++ services/cache.js
@@ -1,68 +1,65 @@
 const mongoose = require('mongoose');
 const redis = require('redis');
 const util = require('util');
-// const keys = require("../config/keys");
 
-(async () => {
-  const client = await redis.createClient({
-    host: '192.168.56.54',
-    port: 6379,
-    retry_strategy: () => 1000,
-  });
+const client = await redis.createClient({
+  host: '192.168.56.54',
+  port: 6379,
+  retry_strategy: () => 1000,
+});
 
-  const client2 = await redis.createClient({
-    host: '192.168.56.55',
-    port: 6379,
-    retry_strategy: () => 1000,
-  });
+const client2 = await redis.createClient({
+  host: '192.168.56.55',
+  port: 6379,
+  retry_strategy: () => 1000,
+});
 
-  console.log('Redis connected');
+console.log('Redis connected');
 
-  client2.HGET = util.promisify(client2.HGET);
-  const exec = mongoose.Query.prototype.exec;
+client2.HGET = util.promisify(client2.HGET);
+const exec = mongoose.Query.prototype.exec;
 
-  mongoose.Query.prototype.cache = function () {
-    this.useCache = true;
-    this.time = 60;
-    this.hashKey = JSON.stringify(this.mongooseCollection.name);
-    console.log(this.time);
+mongoose.Query.prototype.cache = function () {
+  this.useCache = true;
+  this.time = 60;
+  this.hashKey = JSON.stringify(this.mongooseCollection.name);
+  console.log(this.time);
 
-    return this;
-  };
+  return this;
+};
 
-  mongoose.Query.prototype.exec = async function () {
-    if (!this.useCache) {
-      return await exec.apply(this, arguments);
-    }
+mongoose.Query.prototype.exec = async function () {
+  if (!this.useCache) {
+    return await exec.apply(this, arguments);
+  }
 
-    // const key = JSON.stringify({
-    //   ...this.getQuery(),
-    // });
+  // const key = JSON.stringify({
+  //   ...this.getQuery(),
+  // });
 
-    const key = 'bookCache';
-    const cacheValue = await client2.HGET(this.hashKey, key);
+  const key = 'bookCache';
+  const cacheValue = await client2.HGET(this.hashKey, key);
 
-    if (cacheValue) {
-      const doc = JSON.parse(cacheValue);
+  if (cacheValue) {
+    const doc = JSON.parse(cacheValue);
 
-      console.log('Response from Redis');
-      return Array.isArray(doc)
-        ? doc.map((d) => new this.model(d))
-        : new this.model(doc);
-    }
+    console.log('Response from Redis');
+    return Array.isArray(doc)
+      ? doc.map((d) => new this.model(d))
+      : new this.model(doc);
+  }
 
-    const result = await exec.apply(this, arguments);
-    console.log(this.time);
-    client.hset(this.hashKey, key, JSON.stringify(result));
-    client.expire(this.hashKey, this.time);
+  const result = await exec.apply(this, arguments);
+  console.log(this.time);
+  client.hset(this.hashKey, key, JSON.stringify(result));
+  client.expire(this.hashKey, this.time);
 
-    console.log('Response from MongoDB');
-    return result;
-  };
+  console.log('Response from MongoDB');
+  return result;
+};
 
-  module.exports = {
-    clearKey(hashKey) {
-      client.del(JSON.stringify(hashKey));
-    },
-  };
-})();
+module.exports = {
+  clearKey(hashKey) {
+    client.del(JSON.stringify(hashKey));
+  },
+};
