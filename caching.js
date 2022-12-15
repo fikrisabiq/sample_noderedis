f4341bcd1d19e890620c0c7d86af881b362493e8
fikrisabiq <fikrisabiq@gmail.com>
2022-12-15 22:11:01 +0700

    Caching

---
+++ caching.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+// require('./services/cache');
+require('./model/Book');
+
+const app = express();
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://192.168.56.52:27017,192.168.56.53:27017/Books', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+require('./routes/caching')(app);
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Listening on port`, PORT);
+});
